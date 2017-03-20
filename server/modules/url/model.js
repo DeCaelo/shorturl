19112e8a35caeb1a1451668905d8064e6a8c8d0e
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-03-20 11:33:07 +0100

    make unique url in model

--- server/modules/url/model.js
+++ server/modules/url/model.js
@@ -1,7 +1,40 @@
 import mongoose, { Schema } from 'mongoose';
+import crypto from 'crypto';
 
 const UrlSchema = new Schema({
-  longUrl: { type: String, unique: true, required: true }
+  longUrl: { type: String, unique: true, required: true },
+  shortUrl: {
+    type: String,
+    unique: true,
+    maxLength: [6, "Error with the minimum length of 6"]
+  }
+});
+
+/**
+ * Function for make short url
+ */
+
+const UniqueUrl = len =>
+  crypto
+    .randomBytes(Math.ceil(len * (3/4)))
+    .toString('base64')
+    .slice(0, len)
+    .replace(/\+/g, '0')
+    .replace(/\//g, '0');
+
+UrlSchema.pre('save', function(next, done) {
+  this.shortUrl = UniqueUrl(6);
+
+  mongoose.models.Url.findOne({ shortUrl: this.shortUrl })
+    .then(
+      url => {
+        if (url) {
+          this.shortUrl = UniqueUrl(6);
+        }
+      },
+      error => done(error)
+    );
+  next();
 });
 
 export default mongoose.model('Url', UrlSchema);
