3aac08b9c35aa4378ac1457b9b390a129cca5449
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-03-20 13:03:24 +0100

    webpack in env & prod

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import dbConfig from './config/db';
 import middlewareConfig from './config/middleware';
 import { urlRoutes } from './modules';
@@ -7,11 +8,28 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+let mongoConf;
+
+if (process.env.NODE_ENV !== 'production') {
+  const webpackMiddleware = require('webpack-dev-middleware');
+  const webpack = require('webpack');
+  const webpackConfig = require('../webpack.config');
+
+  app.use(webpackMiddleware(webpack(webpackConfig)));
+  mongoConf = 'mongodb://localhost/shorturl';
+} else {
+  app.use(express.static('dist'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
+  mongoConf = process.env.MONGO_URL;
+}
+
 /**
  * DATABASE
  */
 
-dbConfig();
+dbConfig(mongoConf);
 
 /**
  * MIDDLEWARE
