74c8c7db2f1e5cdbbd2cf056b01e1147168ec77d
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-03-20 11:08:50 +0100

    validations in controller

--- server/modules/url/controller.js
+++ server/modules/url/controller.js
@@ -1,12 +1,28 @@
+import { isURL } from 'validator';
 import Url from './model';
 
-export const createShort = async (req, res) => {
+export const createShort = (req, res) => {
   const { longUrl } = req.body;
-  const newUrl = new Url({ longUrl });
 
-  try {
-    return res.status(201).json({ url: await newUrl.save() });
-  } catch (e) {
-    return res.status(e.status).json({ error: true, message: 'Error with Url'});
+  if (!longUrl) {
+    return res.status(400).json({ message: 'You must provided a url' });
+  } else if (!isURL(longUrl)) {
+    return res.status(400).json({ message: 'You must provided a valid url' });
   }
+
+  Url.findOne({ longUrl })
+    .then(
+      url => {
+        if (!url) {
+          const newUrl = new Url({ longUrl });
+          return newUrl.save()
+            .then(
+              u => res.status(201).json({ url: u }),
+              error => res.status(500).json({ error })
+            );
+        }
+        return res.status(200).json({ url });
+      },
+      error => res.status(500).json({ error })
+    )
 }
